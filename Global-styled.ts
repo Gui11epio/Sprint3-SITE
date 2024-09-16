ffc1596b0e479d65aa94933e7feb3e4ca89129e5
Guilherme Laiber <162480837+Gui11epio@users.noreply.github.com>
2024-09-16 10:07:05 -0300

    Final

---
+++ Global-styled.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    *{
+        padding: 0;
+        margin: 0;
+        font-family: Arial, Helvetica, sans-serif;
+        box-sizing: border-box;
+    }
+
+    #root{
+        width: 100vw;
+        min-height: 100vw;
+        display: flex;
+        flex-direction: column;
+    }
+
+    
+`
+
+export default GlobalStyle
